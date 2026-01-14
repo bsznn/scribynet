1d20c55774e5570250d759493dc25f21195fd5d4
Suzan BULGAN <sbulgan@prismamedia.com>
2026-01-14 14:54:10 +0100

    fond de form modifié

--- front/src/pages/auth/Register.jsx
+++ front/src/pages/auth/Register.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../../assets/styles/pages/auth/form.css";
-import fondImage from "../../assets/images/fond/3.jpg";
+import fondImage from "../../assets/images/fond/fond-cafe.jpeg";
 
 
 export default function Register() {
