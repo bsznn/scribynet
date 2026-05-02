d963542efe39ca9cd50960418a105b6936d081c3
Suzan BULGAN <sbulgan@prismamedia.com>
2026-05-02 13:14:39 +0200

    index.html update

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,30 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+// Images LCP critiques à nom stable (sans hash)
+const STABLE_ASSETS = new Set([
+	"grass.jpg",
+	"fond-arbre.jpeg",
+	"fond-book.jpeg",
+	"fond-books.jpeg",
+	"fond-cafe.jpeg",
+	"fond-cat.jpg",
+	"fond-don.jpeg",
+	"fond-profile.jpeg",
+]);
+
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				assetFileNames: (assetInfo) => {
+					if (STABLE_ASSETS.has(assetInfo.name)) {
+						return "assets/[name][extname]";
+					}
+					return "assets/[name]-[hash][extname]";
+				},
+			},
+		},
+	},
 });
