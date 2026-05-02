77f90686377881a57da1dc53960eb3b4f397925a
Suzan BULGAN <sbulgan@prismamedia.com>
2026-05-02 15:10:29 +0200

    congif fix

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,30 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// Images LCP critiques à nom stable (sans hash)
-const STABLE_ASSETS = new Set([
-	"grass.jpg",
-	"fond-arbre.jpeg",
-	"fond-book.jpeg",
-	"fond-books.jpeg",
-	"fond-cafe.jpeg",
-	"fond-cat.jpg",
-	"fond-don.jpeg",
-	"fond-profile.jpeg",
-]);
-
+// https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	build: {
-		rollupOptions: {
-			output: {
-				assetFileNames: (assetInfo) => {
-					if (STABLE_ASSETS.has(assetInfo.name)) {
-						return "assets/[name][extname]";
-					}
-					return "assets/[name]-[hash][extname]";
-				},
-			},
-		},
-	},
 });
